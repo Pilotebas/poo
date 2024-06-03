84d266bd0600bccf2fd17a1c2837b69fa4ae756f
Pilotebas <149727792+Pilotebas@users.noreply.github.com>
2024-06-03 17:33:55 -0500

    feat: :sparkles: Se ha agregado la clase criatura

---
+++ js/Criatura.js
@@ -0,0 +1,17 @@
+class Criatura {
+
+    
+    constructor(nombre, vida, ataque) {
+        // Verifica si se está intentando instanciar directamente la clase Criatura.
+        if (new.target === Criatura) {
+            throw new TypeError("Cannot construct Criatura instances directly");
+        }
+        this.nombre = nombre; // Nombre de la criatura.
+        this.vida = vida; // Vida actual de la criatura.
+        this.vidaMaxima = vida; // Vida máxima de la criatura.
+        this.ataque = ataque; // Ataque de la criatura.
+    }
+}
+
+
+module.exports = Criatura;
